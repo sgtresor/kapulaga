36fb501165c3c5c1c2226a565582e7218ee5c190
sgtresor <sgtresor7@gmail.com>
2026-01-02 07:05:24 +0700

    standard movement

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -2,3 +2,8 @@ export const PORT = 9208;
 
 // How many times per second the server updates
 export const TICK_RATE = 30;
+
+export type InputPayload = {
+  x: number; // Left/Right (-1 to 1)
+  y: number; // Forward/Backward (-1 to 1)
+};
